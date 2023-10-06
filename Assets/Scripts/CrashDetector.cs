5474e6f1559f66dd7be279153cc7f1aa572ef1a8
Will Whittenton <willwhittenton@gmail.com>
2023-10-06 16:31:15 -0500

    added crash and finish scene reload

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class CrashDetector : MonoBehaviour
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("You crashed!");
+            SceneManager.LoadScene(0);
         }
     }
 }
